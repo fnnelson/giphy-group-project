7e1273b70dee172265f6ab0ba473648bdd533c10
Forrest Nelson <forrestnnelson@gmail.com>
2023-08-24 15:38:38 -0500

    added reducer

--- src/index.js
+++ src/index.js
@@ -10,22 +10,31 @@ import { takeLatest, put } from 'redux-saga/effects'
 import axios from 'axios';
 
 
+const giphyList = (state = [], action) => {
+    if (action.type === 'GET_GIPHY_DATA') {
+        return action.payload;
+    }
+    return state;
+}
 
 
 function* getGiphy(action) {
+    const giphySearch = action.payload
     // addGiphyData works like response, addGiphyData is response.data
-    const addGiphyData = yield axios.get('/')
+    const addGiphyData = yield axios.get(`/api/giphy/${giphySearch}`)
+    console.log("")
+    yield put({ type: 'GET_GIPHY_DATA', payload: addGiphyData.data })
 }
 
 function* rootSaga() {
-yield takeLatest('FETCH_SEARCH', getGiphy)
+    yield takeLatest('FETCH_SEARCH', getGiphy)
 }
 
 const sagaMiddleware = createSagaMiddleware();
 
 const storeInstance = createStore(
     combineReducers({
-
+        giphyList
     }),
     applyMiddleware(
         logger,
